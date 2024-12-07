363111f4bb66f6af9d0f607f353f6b84461dfbf3
Kevin Almeida <kevin.almeidafsa@gmail.com>
2024-12-07 11:07:47 -0300

    feat: implement user authentication with Redux

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import Cart from "../cart/index";
@@ -6,18 +7,43 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 
+import { loginUser, logoutUser } from "../../redux/user/actions";
+
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const dispatch = useDispatch();
+
+  console.log(currentUser);
+
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+    dispatch(
+      loginUser({
+        name: "John Doe",
+        email: "john@studio.com.br",
+      })
+    );
+  };
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser());
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}>Sair</div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
+
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
